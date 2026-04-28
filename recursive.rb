1f72d44bd02c54989f46a13eef4859766556006d
William <William@talbott.ws>
2026-04-28 10:41:17 -0400

    Recursive Merge Sort

---
+++ recursive.rb
@@ -0,0 +1,53 @@
+def fibs_rec(number)
+
+  puts ("recursion! #{number}")
+  
+  if number <= 1
+    nil
+  elsif number == 2
+    [0, 1]
+  else
+    smaller = fibs_rec(number - 1)
+    smaller << (smaller[-1] + smaller[-2])
+  end
+
+end
+
+def merge_sort(array)
+
+  if array.length <= 1
+    return array
+  else
+    middle = array.length / 2
+    front = merge_sort(array[0...middle])
+    back = merge_sort(array[middle..-1])
+
+    merge_loop(front, back)
+    
+  end
+
+end
+
+def merge_loop(front, back)
+  result = []
+
+  until front.empty? or back.empty?
+    if front[0] <= right[0]
+      result << front.shift
+    else
+      result << back.shift
+    end
+  end
+
+  result + front + back
+  
+end
+
+puts merge_sort([3, 2, 1, 13, 8, 5, 0, 1]) # -> [0, 1, 1, 2, 3, 5, 8, 13]
+
+  #Main
+  #
+  #
+  #
+  #Smaller = Recursion
+  #Apply change
